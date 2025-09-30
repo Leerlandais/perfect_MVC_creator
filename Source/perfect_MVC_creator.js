5206564c884ffbdc1352b21f96153ca8f6ef5ea7
leerlandais <leerlandais@gmail.com>
2025-09-30 19:33:31 +0200

    added the new table sorter

--- Source/perfect_MVC_creator.js
+++ Source/perfect_MVC_creator.js
@@ -152,7 +152,11 @@ config.php
 {% block stylesheet %}
     <\link href="../dist/tailwind.min.css" rel="stylesheet">
 {% endblock %}
-{% block bodyClass %}h-screen bg-amber-50{% endblock %}`;
+{% block bodyClass %}h-screen bg-amber-50{% endblock %}
+
+{% block javascript %}
+<script src="{{ PUB_DIR }}scripts/tableSorter.js"></script>
+{% endblock %}`;
                     fs.writeFileSync(`${projName}/view/template.html.twig`, tempTwig);
                     console.error(`Created template.html.twig`);
                 } catch (error) {
@@ -862,6 +866,48 @@ console.error(`Created ConnectionController.php`);
                 }catch(error){
                     console.error(`Error occurred: ${error.message}`);
                 }
+                try {
+                    const tableSorter = `const currentOrder = new WeakMap();
+
+function sortTable(table, columnIndex) {
+
+    const tbody = table.tBodies[0];
+    const rows = Array.from(tbody.querySelectorAll("tr"));
+    let state = currentOrder.get(table) || { column: null, order: 'asc' };
+
+    if (state.column === columnIndex) {
+        state.order = state.order === 'asc' ? 'desc' : 'asc';
+    } else {
+        state.order = 'asc';
+    }
+    state.column = columnIndex;
+    
+    currentOrder.set(table, state);
+
+    const sortedRows = rows.sort((a, b) => {
+        const A = a.children[columnIndex].textContent.trim();
+        const B = b.children[columnIndex].textContent.trim();
+        if (state.order === 'asc') {
+            return A.localeCompare(B, undefined, { numeric: true });
+        } else {
+            return B.localeCompare(A, undefined, { numeric: true });
+        }
+    });
+
+    tbody.innerHTML = "";
+    sortedRows.forEach(row => tbody.appendChild(row));
+}
+document.querySelectorAll("table").forEach(table => {
+    table.querySelectorAll("th").forEach((th, index) => {
+        th.addEventListener("click", () => sortTable(table, index));
+    });
+});`
+                    fs.writeFileSync(`${projName}/public/scripts/tableSorter.js`, tableSorter);
+                    console.error(`Created tableSorter.js`);
+                }catch (error) {
+                    console.error(`Error occurred: ${error.message}`);
+                }
+
                 try {
 
                     const tailConf = `/** @type {import('tailwindcss').Config} */
